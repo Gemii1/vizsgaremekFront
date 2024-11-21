219b3a011370b00e3554e42d43d838e12c89a604
fidrer1 <nagykristof215@gmail.com>
2024-11-21 15:19:57 +0100

    Regisztrációs változások

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,25 +1,29 @@
 import styles from './Registration.module.css';
 import Button from '@mui/material/Button';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-
-
+import Tabs from '@mui/joy/Tabs';
+import TabList from '@mui/joy/TabList';
+import Tab from '@mui/joy/Tab';
+import TabPanel from '@mui/joy/TabPanel';
 
 function Registration({close}) {
-    return(
+    return (
         <>
             <div className={styles.container}>
-                <h3>Válassz felhasználót</h3>
-                <div className={styles.userTypePicker}>
-                    Kliensként
-                    {<ArrowForwardIosIcon className={styles.arrow} fontSize='small'/>}
-                </div>
-                <div className={styles.userTypePicker}>
-                    Edzőként 
-                    {<ArrowForwardIosIcon className={styles.arrow} fontSize='small'/>}
-                </div>
-
-                <div></div>
-                <Button className={styles.closeButton} variant="outlined" color="error" onClick={()=>{close()}}>Close</Button>
+                <Tabs >
+                    <TabList tabFlex="auto">
+                        <Tab>Edzőként</Tab>
+                        <Tab>Kliensként</Tab>
+                    </TabList>
+                    <TabPanel value={0}>
+                        <b>First</b> tab panel
+                    </TabPanel>
+                    <TabPanel value={1}>
+                        <b>Second</b> tab panel
+                    </TabPanel>
+                </Tabs>
+                <Button className={styles.closeButton} variant="outlined" color="error" onClick={() => {
+                    close()
+                }}>Close</Button>
             </div>
         </>
     )
@@ -28,3 +32,18 @@ function Registration({close}) {
 
 
 export default Registration;
+
+/*
+<h3>Válassz felhasználót</h3>
+<div className={styles.userTypePicker}>
+    Kliensként
+    {<ArrowForwardIosIcon className={styles.arrow} fontSize='small'/>}
+</div>
+<div className={styles.userTypePicker}>
+    Edzőként
+    {<ArrowForwardIosIcon className={styles.arrow} fontSize='small'/>}
+</div>
+
+<div></div>
+
+ */
