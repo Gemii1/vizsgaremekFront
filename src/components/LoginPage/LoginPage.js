3313a6c277bd146a361c04946b8651d4b10e58b3
fidrer1 <nagykristof215@gmail.com>
2024-11-24 21:20:48 +0100

    Regisztráció kb kész, Bejelntkezést elkezdtem Nested Modellel

---
+++ src/components/LoginPage/LoginPage.js
@@ -0,0 +1,16 @@
+
+import Modal from '@mui/material/Modal';
+import styles from './LoginPage.module.css'
+
+function LoginPage(){
+    console.log("RAWR")
+
+    return (
+        <>
+
+
+        </>
+    )
+}
+
+export default LoginPage;
